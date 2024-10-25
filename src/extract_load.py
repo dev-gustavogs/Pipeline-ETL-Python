f6b4c80aad6d91c147e15196bff1aac8e3f96b59
Gustavo Santos <gustavogomes.bsb68@gmail.com>
2024-10-25 09:20:55 -0300

    finishing all the extract and conecting with the sql server postgres/render

--- src/extract_load.py
+++ src/extract_load.py
@@ -8,8 +8,21 @@ import os
 #import das variaveis de ambiente 
 commodities = ['CL=F', 'GC=F', 'SI=F']
 
-def buscar_dados_commodities(simbolo, periodo='5y', intevalo='1d'):
-  ticker = yf.Ticker('CL=F')
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine =  create_engine(DATABASE_URL)
+
+def buscar_dados_commodities(simbolo, periodo='5d', intevalo='1d'):
+  ticker = yf.Ticker(simbolo)
   dados=ticker.history(period=periodo, interval=intevalo)[['Close']]
   dados['simbolo'] = simbolo
   return dados
@@ -21,9 +34,12 @@ def buscar_todos_dados_commodities():
     todos_dados.append(dados)
   return pd.concat(todos_dados)
 
+def salvar_no_postgres(df, schema='public'):
+  df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+
 if __name__ == "__main__":
   dados_concatenados =  buscar_todos_dados_commodities()
-  print(dados_concatenados)
+  salvar_no_postgres(dados_concatenados, schema='public')
   
 #pega a cotação dos meus ativos
 
