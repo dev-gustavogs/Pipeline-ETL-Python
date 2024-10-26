273d6097eae578dae0aae02dc3f301d88bb320cd
Gustavo Santos <gustavogomes.bsb68@gmail.com>
2024-10-26 10:05:58 -0300

    feat:trying connect tables on database

---
+++ datawarehouse/models/staging/stg_commodities.sql
@@ -0,0 +1,21 @@
+-- import
+
+with source as (
+  select
+    "Date",
+    "Close",
+    simbolo
+  from {{ source ('databaseteste_44aj', 'commodities') }}
+),
+
+--renamed
+renamed as (
+  select
+    cast("Date" as date) as data,
+    "Close" as valor_fechamento,
+    simbolo
+  from source
+)
+--select * from 
+
+select * from renamed
---
+++ datawarehouse/models/staging/stg_movimentacao_commodities.sql
@@ -0,0 +1,22 @@
+-- models/staging/stg_movimentacao_commodities.sql
+
+with source as (
+    select
+        Date,
+        Symbol,
+        action,
+        quantity
+    from 
+        {{ source('databaseteste_44aj', 'movimentacao_commodities') }}
+),
+
+renamed as (
+    select
+     cast(date as date) as data,
+        Symbol as simbolo,
+        action as acao,
+        quantity as quantidade
+    from source
+)
+
+select * from renamed
